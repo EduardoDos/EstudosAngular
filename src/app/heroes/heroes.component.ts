4c000f48cffa20860c1f6f7465929692054292c7
Eduardo de Oliveira Siqueira <eduardo.siqueira@engcomp.local>
2018-02-15 14:56:00 -0200

    finalizando Fix the

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,6 +22,20 @@ export class HeroesComponent implements OnInit {
     .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string) {
+    name = name.trim();
+    if(!name) {return;}
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero) {
+    this.heroes = this.heroes.filter(h => h!==hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+  
   /*
     nomeDaFunção(parametro: TipoDoParametro): tipoDaFunção {
     this.selectedHero = hero;
